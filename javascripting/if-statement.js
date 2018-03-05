e00d3fd555b2760e21a7fc1e6a14f1de7b4816f8
juandados <juandados@gmail.com>
2018-03-05 16:37:59 -0500

    working with if stament and others

---
+++ javascripting/if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+if (fruit.length > 5){
+  var out = 'The fruit name has more than five characters.';
+} else {
+  var out = 'The fruit name has five characters or less.';
+}
+console.log(out);
